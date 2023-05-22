a0347383c0fcde5468c9a255fca8f1a9a6a8d4c9
Zeff01 <jzeffsomera@gmail.com>
2023-05-22 19:50:07 +0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,256 @@
+import * as React from "react";
+import Map, { Marker } from "react-map-gl";
+import { Popup } from "react-map-gl";
+import FmdGoodIcon from "@mui/icons-material/FmdGood";
+import { useState } from "react";
+import StarIcon from "@mui/icons-material/Star";
+import axios from "axios";
+import { format } from "timeago.js";
+import { NavigationControl } from "react-map-gl";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Register from "./Components/Register/Register";
+import Login from "./Components/Login/Login";
+
+const pinAddSuccess = () => {
+  toast.success("Added pin!");
+};
+
+const userNotLoggedIn = () => {
+  toast.warning("Login to account to set pins!");
+};
+const userLoggedOut = (userS) => {
+  toast.warning("Logout from " + userS);
+};
+
+const pinAddFailure = () => {
+  toast.error("Couldn't add pin. Please fill all data");
+};
+
+function App() {
+  const myStorage = window.localStorage;
+
+  const [pins, setPins] = useState([]);
+  const [newPlace, setNewPlace] = useState(null);
+
+  const [title, setTitle] = useState(null);
+  const [descr, setDescr] = useState(null);
+  const [rating, setRating] = useState(1);
+
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const [viewPort, setViewPort] = useState({
+    longitude: 12.4,
+    latitude: 37.8,
+    zoom: 14,
+  });
+
+  const [currentPlaceId, setCurrentPlaceId] = useState(null);
+
+  React.useEffect(() => {
+    const getPins = async () => {
+      try {
+        const responce = await axios.get("/pins");
+        setPins(responce.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getPins();
+  }, []);
+
+  const handleMarkerClicked = (id, lat, long) => {
+    setCurrentPlaceId(id);
+  };
+
+  const handleAddClick = (e) => {
+    let lat = e.lngLat.lat;
+    let long = e.lngLat.lng;
+    setNewPlace({
+      lat: lat,
+      lng: long,
+    });
+  };
+
+  const handlePinSubmit = async (e) => {
+    e.preventDefault();
+
+    const newPin = {
+      userName: currentUser,
+      title: title,
+      description: descr,
+      rating: rating,
+      lat: newPlace.lat,
+      long: newPlace.lng,
+    };
+    console.log("newPin:", newPin);
+
+    try {
+      if (!currentUser) {
+        userNotLoggedIn();
+      } else {
+        const response = await axios.post("/pins", newPin);
+        setPins([...pins, response.data]);
+        setNewPlace(null);
+        pinAddSuccess();
+        setRating(1);
+        setDescr(null);
+        setTitle(null);
+      }
+    } catch (err) {
+      console.log(err);
+      pinAddFailure();
+    }
+  };
+
+  const handleLogout = () => {
+    myStorage.removeItem("user");
+    userLoggedOut(currentUser);
+    setCurrentUser(null);
+  };
+
+  const [showRegister, setShowRegister] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+
+  return (
+    <div className="app">
+      <Map
+        container={"map"}
+        projection={"globe"}
+        initialViewState={{ viewPort }}
+        mapboxAccessToken={process.env.REACT_APP_TOKEN}
+        style={{ width: "100vw", height: "100vh" }}
+        mapStyle="mapbox://styles/jzeff01/clhvr48iz00rn01poatjtbxks"
+        onDblClick={handleAddClick}
+      >
+        <ToastContainer position="top-left" theme="dark" />
+        <NavigationControl />
+        {pins.map((p) => (
+          <>
+            <Marker longitude={p.long} latitude={p.lat} anchor="center">
+              <FmdGoodIcon
+                className="icon"
+                onClick={() => handleMarkerClicked(p._id, p.lat, p.long)}
+                style={{
+                  fontSize: viewPort.zoom * 2,
+                  color: p.userName === currentUser ? "tomato" : "slateblue",
+                }}
+              />
+            </Marker>
+
+            {p._id === currentPlaceId && (
+              <Popup
+                longitude={p.long}
+                latitude={p.lat}
+                closeOnClick={false}
+                closeOnMove={false}
+                onClose={() => setCurrentPlaceId(null)}
+                anchor="left"
+                className="popup"
+              >
+                <div className="card">
+                  <label>Place</label>
+                  <h4 className="place">{p.title}</h4>
+                  <label>Review</label>
+                  <p className="descr">{p.descr}</p>
+                  <label>Rating</label>
+                  <div className="stars">
+                    {Array(p.rating).fill(<StarIcon className="star" />)}
+                  </div>
+                  <label>Information</label>
+                  <div className="info">
+                    <span className="username">
+                      Created by <b>{p.userName}</b>
+                    </span>
+                    <span className="date">{format(p.createdAt)}</span>
+                  </div>
+                </div>
+              </Popup>
+            )}
+          </>
+        ))}
+        {newPlace && (
+          <Popup
+            longitude={newPlace.lng}
+            latitude={newPlace.lat}
+            closeOnClick={false}
+            closeOnMove={false}
+            onClose={() => setNewPlace(null)}
+            anchor="left"
+          >
+            <div>
+              <form onSubmit={handlePinSubmit}>
+                <label>Title</label>
+                <input
+                  placeholder="Enter a title..."
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Review</label>
+                <textarea
+                  placeholder="Say something about this place..."
+                  onChange={(e) => setDescr(e.target.value)}
+                />
+                <label>Rating</label>
+                <select onChange={(e) => setRating(e.target.value)}>
+                  <option value="1">1 </option>
+                  <option value="2">2 </option>
+                  <option value="3">3 </option>
+                  <option value="4">4 </option>
+                  <option value="5">5 </option>
+                </select>
+                <button className="submitButton" type="submit">
+                  Add Pin
+                </button>
+              </form>
+            </div>
+          </Popup>
+        )}
+      </Map>
+
+      <div className="footer">
+        <div className="footer_down">
+          {currentUser ? (
+            <button className="button logout" onClick={handleLogout}>
+              Log out
+            </button>
+          ) : (
+            <div className="buttons">
+              <button
+                className="button login"
+                onClick={() => {
+                  setShowLogin(true);
+                  setShowRegister(false);
+                }}
+              >
+                Login
+              </button>
+              <button
+                className="button register"
+                onClick={() => {
+                  setShowRegister(true);
+                  setShowLogin(false);
+                }}
+              >
+                Register
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+
+      {showRegister && <Register setShowRegister={setShowRegister} />}
+      {showLogin && (
+        <Login
+          setShowLogin={setShowLogin}
+          myStorage={myStorage}
+          setCurrentUser={setCurrentUser}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
