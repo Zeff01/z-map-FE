d76ff6dad96df616b44036cbd8c691344c7bbbbb
Zeff01 <jzeffsomera@gmail.com>
2023-06-02 13:08:22 +0800

    add browserlist

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  presets: ["@babel/preset-env", "@babel/preset-react"],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: "browserslist",
+      },
+    ],
+    "@babel/preset-react",
+  ],
 };
